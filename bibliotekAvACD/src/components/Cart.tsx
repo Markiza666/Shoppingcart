dea970aae893c7a28ad2317e5678d4d159ad2691
Markiza666 <3ggjs24erikma@folkuniversitetet.nu>
2025-02-14 10:11:49 +0100

    Stylat korgen

--- bibliotekAvACD/src/components/Cart.tsx
+++ bibliotekAvACD/src/components/Cart.tsx
@@ -1,18 +1,26 @@
-// import React from 'react';
 import { useCart } from './UseCart';
+import { useState } from "react"
 
 export function Cart() {
     const { cart } = useCart(); // Hämtar cart från context
+        const [isDropdownVisible, setIsDropdownVisible] = useState(false);
 
     return (
         <div className="cart-container">
             <h2>Cart</h2>
-            <div id="books">{cart.length}</div> {/* Visar antalet varor */}
-            <ul>
-                {cart.map(item => (
-                    <li key={item.id}>{item.title}</li> // Visar titeln för varje produkt
-                ))}
-            </ul>
+            <div id="books" 
+            onMouseEnter={() => setIsDropdownVisible(true)} 
+            onMouseLeave={() => setIsDropdownVisible(false)}
+            >
+                {cart.length}
+            </div> {/* Visar antalet varor */}
+            {isDropdownVisible && ( // Visa dropdown endast när isDropdownVisible är true
+                <ul className="dropdown">
+                    {cart.map(item => (
+                        <li key={item.id}>{item.title}</li> // Visar titeln för varje produkt
+                    ))}
+                </ul>
+            )}
         </div>
     ); 
 }
